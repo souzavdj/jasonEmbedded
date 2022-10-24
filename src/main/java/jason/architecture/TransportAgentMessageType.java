f4bce9520e4af33632e6a2508ae1f45af3f4ac54
Adelmo Lima <116198540+adelmo-lima@users.noreply.github.com>
2022-10-24 19:54:47 -0300

    Update TransportAgentMessageType.java

--- src/main/java/jason/architecture/TransportAgentMessageType.java
+++ src/main/java/jason/architecture/TransportAgentMessageType.java
@@ -7,7 +7,12 @@ public enum TransportAgentMessageType {
     PREDATION(1, "PREDATION"),
     MUTUALISM(2, "MUTUALISM"),
     INQUILINISM(3, "INQUILINISM"),
-
+    
+    //Clonagem begin
+    CLONAGEM_SMA(6, "CLONAGEM SMA"),
+    CLONAGEM_AGENT(7, "CLONAGEN AGENTE"),
+    //Clonagem end
+    
     CAN_TRANSFER(4, "CAN TRANSFER THE AGENT(S)"),
     CAN_KILL(5, "CAN KILL THE AGENT(S)");
 
