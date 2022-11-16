bd040f543464c3189f287c734784c6a36ff79db8
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-11-16 16:47:06 -0300

    JAVINO - nova versão (1.4)

--- bin/install_maven_dependencies.sh
+++ bin/install_maven_dependencies.sh
@@ -15,7 +15,8 @@ mvn install:install-file -Dfile=../lib/UmlGraph.jar -DgroupId=mastransport.UmlGr
 #mvn install:install-file -Dfile=../lib/javino_stable_1.1.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
 #mvn install:install-file -Dfile=../lib/javino_stable_1.3.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.3 -Dpackaging=jar -DgeneratePom=true
 #mvn install:install-file -Dfile=../lib/javino_stable_1.3.9.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.3.9 -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=../lib/javino_stable_1.3.10-chonOS.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.3.9 -Dpackaging=jar -DgeneratePom=true
+#mvn install:install-file -Dfile=../lib/javino_stable_1.3.10-chonOS.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.3.9 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=../lib/javino_stable_1.4-chonOS.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.4 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=../lib/contextnet-2.5.jar -DgroupId=br.pucrio.inf.lac -DartifactId=contextnet -Dversion=2.5 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=../lib/contextnet-2.7.jar -DgroupId=br.pucrio.inf.lac -DartifactId=contextnet -Dversion=2.7 -Dpackaging=jar -DgeneratePom=true
 
